cddd0428d637c2dd318c66343ff316a3e08e3446
Drake Costa <saeris@comcast.net>
2016-12-25 07:03:46 -0800

    feat(router): Add Top-Level Routes
    Added top-level routes to app.js.

--- src/app.js
+++ src/app.js
@@ -7,9 +7,64 @@ export class App {
     config.title = `Saeris.io`
     config.options.root = `/`
     config.map([
-      { route: [``], name: `home`, moduleId: `app/routes/public/home/home`, nav: false, title: `Home`},
-      { route: [`error`], name: `error`, moduleId: `app/routes/public/error/error`, nav: false, title: `Error`},
-      { route: [`test`], name: `test`, moduleId: `app/routes/public/test/test`, nav: false, title: `Test`}
+      {
+        route: [``, `home`],
+        name: `home`,
+        moduleId: `app/routes/public/home/home`,
+        nav: true,
+        title: `Home`,
+        settings: {
+          icon: `home`
+        }
+      }, {
+        route: [`blog`],
+        name: `blog`,
+        moduleId: `app/routes/public/blog/blog`,
+        nav: true,
+        title: `Blog`,
+        settings: {
+          icon: `blog`
+        }
+      }, {
+        route: [`projects`],
+        name: `projects`,
+        moduleId: `app/routes/public/projects/projects`,
+        nav: true,
+        title: `Projects`,
+        settings: {
+          icon: `code`
+        }
+      }, {
+        route: [`photography`],
+        name: `photography`,
+        moduleId: `app/routes/public/photography/photography`,
+        nav: true,
+        title: `Photography`,
+        settings: {
+          icon: `photo`
+        }
+      }, {
+        route: [`resume`],
+        name: `resume`,
+        moduleId: `app/routes/public/resume/resume`,
+        nav: true,
+        title: `Resume`,
+        settings: {
+          icon: `id-card-o`
+        }
+      }, {
+        route: [`error`],
+        name: `error`,
+        moduleId: `app/routes/public/error/error`,
+        nav: false,
+        title: `Error`
+      }, {
+        route: [`test`],
+        name: `test`,
+        moduleId: `app/routes/public/test/test`,
+        nav: false,
+        title: `Test`
+      }
     ])
 
     config.mapUnknownRoutes(`app/routes/public/error/error`)
