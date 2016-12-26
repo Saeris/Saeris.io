eeb202a02d826cc7d92732c09a8f4fbe05cd5818
Drake Costa <saeris@comcast.net>
2016-12-25 21:10:08 -0800

    feat(nav): Pull Author Details from GitHub via GraphQL/Apollo
    Added Apollo graphql support to app-nav. For now it pulls author data from GitHub. This will later be refactored into the profile model.

--- src/app/components/core/app-nav/app-nav.js
+++ src/app/components/core/app-nav/app-nav.js
@@ -1,10 +1,23 @@
-import { bindable, customElement, containerless } from 'aurelia-framework'
+import { inject, bindable, customElement, containerless } from 'aurelia-framework'
+import Apollo from '../../../services/apollo'
+import query from './app-nav.graphql'
 import './app-nav.scss'
 
 @customElement(`app-nav`)
 @containerless
+@inject(Apollo)
 export class AppNav {
   @bindable router
-  constructor() {
+  constructor(apollo) {
+    this.client = apollo.client
+  }
+
+  async attached() {
+    let profile = await this.client.query({query: query})
+    this.profile = {
+      name: profile.data.viewer.name,
+      location: profile.data.viewer.location,
+      picture: profile.data.viewer.avatarURL
+    }
   }
 }
