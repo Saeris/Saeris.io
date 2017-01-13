6c529209ac9353a2c2bdd2670c1a1a961ca1c1ec
Drake Costa <Saeris@comcast.net>
2017-01-13 08:56:15 -0800

    feat(flickr): Create Flickr API Service, Album and Photo Models
    Created a basic Flickr API service class which can retrieve all albums with included photos for a given user or the default user defined in app.config.
    
    Added support for automatic local development logging with Aurelia Log Manager. Now development logs will automatically be disabled when serving from a host other than localhost.
    
    Disabled App-Nav graphql code, because GitHub's graphql api is not currently public and GitHub will automatically strip out personal access tokens from commits. An alternative solution will need to be found in the mean time.

--- src/app/services/flickr.js
+++ src/app/services/flickr.js
@@ -3,6 +3,13 @@ import Album from '../models/album'
 import Photo from '../models/photo'
 import config from '../../config/app.config'
 
+/**
+ * TODO:
+ * - Add new methods to retrieve more data from Flickr
+ * - Integrate with Redux (persist results to store)
+ * - Memoize all fetch operations based on store lookups
+ */
+
 export default class Flickr {
   constructor() {
     this.log = LogManager.getLogger(`Saeris.io/${this.constructor.name}`)
@@ -11,46 +18,65 @@ export default class Flickr {
     this.user = config.flickr.user
   }
 
+  /**
+   * Generic fetch method. Generates REST URL from provided method and options,
+   * performs the fetch request, and returns the results as JSON.
+   */
   async request(method, options, endpoint = this.endpoint, key = this.key ) {
-    this.log.debug(`Fetching request using: '${method}' with key: '${key}'`, options)
     let params = ``
     Object.entries(options).forEach(([k, v]) => {
       Array.isArray(v) ? params = `${params}&${k}=${v.join()}` : params = `${params}&${k}=${v}`
     })
-    const request = `${endpoint}?method=${method}&api_key=${key}${params}&format=json&nojsoncallback=1`
-    return await fetch(request).then(response => response.json(), error => this.log.error(`Failed to fetch request: '${request}'`, error))
+    const query = `${endpoint}?method=${method}&api_key=${key}${params}&format=json&nojsoncallback=1`
+    try {
+      this.log.debug(`Fetching request: '${query}'`, options)
+      const json = await fetch(query).then(async response => await response.json())
+      this.log.debug(`Received json:`, json)
+      return await json
+    } catch (error) {
+      this.log.error(`Failed to complete request.`, error)
+    }
   }
 
-  async getPhotos(album, user = this.user) {
-    this.log.debug(`3. Getting Photos for album: '${album}'`)
+  /**
+   * Returns an array containing all the albums (and it's photos) for the given user.
+   */
+  async getAlbums(user = this.user) {
+    const options = {
+      user_id: user,
+      primary_photo_extras: [`url_m`, `url_o`]
+    }
+    try {
+      this.log.debug(`Getting Albums for user: '${user}'...`, options)
+      const results = await this.request(`flickr.photosets.getList`, options)
+      const albums = await Promise.all(
+        results.photosets.photoset.map(async album => new Album(album, await this.getAlbumPhotos(album.id)))
+      )
+      this.log.debug(`Successfully retrieved albums!`, albums)
+      return albums
+    } catch (error) {
+      this.log.error(`Failed to retrieve albums.`, error)
+      return []
+    }
+  }
+
+  /**
+   * Returns an array of all the photos belonging to the given album of the given user.
+   */
+  async getAlbumPhotos(album, user = this.user) {
     const options = {
       user_id: user,
       photoset_id: album,
       extras: [`tags`, `url_m`, `url_o`]
     }
-    return await this.request(`flickr.photosets.getPhotos`, options)
-      .then(async response => {
-        this.log.debug(`4. Successfully fetched photos.`, response)
-        return await response.photoset.photo.map(photo => new Photo(photo))
-      }, error => this.log.error(`Failed to fetch photos.`, error))
-  }
-
-  async getAlbums(user = this.user) {
-    this.log.debug(`1. Getting Albums for user: '${user}'`)
-    const options = {
-      user_id: user,
-      primary_photo_extras: [`url_m`, `url_o`]
+    try {
+      this.log.debug(`Getting Photos for album: '${album}'...`, options)
+      const results = await this.request(`flickr.photosets.getPhotos`, options)
+      const photos = await Promise.all(results.photoset.photo.map(photo => new Photo(photo)))
+      this.log.debug(`Successfully retrieved photos!`, photos)
+      return photos
+    } catch (error) {
+      this.log.error(`Failed to retrieve Photos.`, error)
     }
-    return await this.request(`flickr.photosets.getList`, options)
-      .then(async response => {
-        this.log.debug(`2. Successfully fetched albums.`, response)
-        let albums = await response.photosets.photoset.map(async album =>  {
-          let photos = await this.getPhotos(album.id).then(res => res)
-          let result = new Album(album, photos)
-          this.log.debug(`5. Successfully created album.`, result)
-          return result
-        })
-        return albums
-      }, error => this.log.error(`Failed to fetch albums.`, error))
   }
 }
