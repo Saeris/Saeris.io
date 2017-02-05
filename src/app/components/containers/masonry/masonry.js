b3bc873832b7fddb77549250ea42b948f25d343e
Drake Costa <Saeris@comcast.net>
2017-02-05 14:25:27 -0800

    feat(gallery): Progress commit on Gallery Route

--- src/app/components/containers/masonry/masonry.js
+++ src/app/components/containers/masonry/masonry.js
@@ -5,6 +5,10 @@ import Nested from '../../../services/nested'
 export class Masonry {
   @bindable album
   @children(`.box`) boxes
+
+  classes = [`size31`, `size21`, `size32`, ``, `size23`, `size12`, `size13`]
+  ratios = [3, 2, 1.5, 1, 0.666, 0.5, 0.333]
+
   constructor() {
     this.log = LogManager.getLogger(`Saeris.io/${this.constructor.name}`)
   }
@@ -13,15 +17,35 @@ export class Masonry {
     this.nested = new Nested(this.container, {minWidth: 200, gutter: 0, animate: false})
   }
 
-  getAspectRatio(w, h, r) {
-    return `${w / r}:${h / r}`
+  detached() {
+    this.nested.destroy()
   }
 
   setBoxSize(photo) {
-    photo.exif
+    const width = photo.width
+    const height = photo.height
+    const ratio = width / height
+    const matchedRatio = parseFloat(this.closestRatio(ratio, this.ratios))
+    let className = this.classes[this.ratios.indexOf(matchedRatio)]
+    if (matchedRatio === 1) {
+      let coinflip = Math.floor(Math.random() * 2)
+      if (coinflip) {
+        className = `size22`
+      }
+    }
+    return className
   }
 
-  detached() {
-    this.nested.destroy()
+  closestRatio(num, arr) {
+    let curr = arr[0]
+    let diff = Math.abs(num - curr)
+    for (let val = 0; val < arr.length; val++) {
+      let newdiff = Math.abs(num - arr[val])
+      if (newdiff < diff) {
+        diff = newdiff
+        curr = arr[val]
+      }
+    }
+    return curr
   }
 }
