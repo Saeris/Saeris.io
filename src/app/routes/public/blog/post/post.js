13eab736445c101c00f34766dd772402bdb1b7e2
Drake Costa <saeris@comcast.net>
2017-02-20 12:04:04 -0800

    feat(blog) Progress commit for Blog Route

--- src/app/routes/public/blog/post/post.js
+++ src/app/routes/public/blog/post/post.js
@@ -2,8 +2,23 @@ import { LogManager } from "aurelia-framework"
 import './post.scss'
 
 export class Post {
+  src = ``
+
   constructor() {
     this.log = LogManager.getLogger(`Saeris.io/${this.constructor.name}`)
+  }
+
+  activate(params) {
+    this.id = params.id
+  }
+
+  bind() {
+    this.log.debug(`Fetching remote resources...`)
+    try {
+      this.log.debug(`Successfully retrieved remote resources.`)
+    } catch (error) {
+      this.log.error(`Failed to fetch remote resources.`, error)
+    }
   }
 
   attached() {
